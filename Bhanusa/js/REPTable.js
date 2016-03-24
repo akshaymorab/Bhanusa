10debe42d0992b932a3bf7c0edebe99eea68d26b
akshaymorab <akshaymorab@gmail.com>
2016-03-24 17:24:16 +0530

    MultipleSearchUpdate

--- Bhanusa/js/REPTable.js
+++ Bhanusa/js/REPTable.js
@@ -3,7 +3,7 @@ var id;
 function repotable() {
     closediv();
     $('#divRpot').show();
-    $('#divSearch').show();
+    $('#divSearch').hide();
 }
 function FUC(id) {
 
@@ -40,6 +40,8 @@ function FUC(id) {
     if (conString == "tblDC" || conString == "tblCompany" || conString == "tblPO") {
         $("#tblRepoStock").hide();
         $("#tblRepo").show();
+        $('#divSearch').show();
+        openSearch();
         var fin = conString;
         try {
             $.ajax({
@@ -81,6 +83,7 @@ function FUC(id) {
     else {
         $("#tblRepo").hide();
         $("#divSearch").show();
+        openSearch();
         var fin = conString;
         try {
             $.ajax({
@@ -108,17 +111,7 @@ function FUC(id) {
 
                 for (var i = 0; i <= rowLength - 1; i++) {
                     rwDt = tblStockDt[i].split(';');
-                    $("#tblRepoStock tbody").append(
-                        '<tr>' +
-                        '<td><div style="color:black; class="srno" right:inherit">' + rwDt[0] + '</div></td>' +
-                        '<td><div style="color:black; class="mdno" right:inherit">' + rwDt[1] + '</div></td>' +
-                        '<td><div style="color:black; right:inherit">' + rwDt[2] + '</div></td>' +
-                        '<td><div style="color:black; right:inherit">' + rwDt[3] + '</div></td>' +
-                        '<td><div style="color:black; right:inherit">' + rwDt[4] + '</div></td>' +
-                        '<td><div style="color:black; right:inherit">' + rwDt[5] + '</div></td>' +
-                        '<td><div style="color:black; right:inherit">' + rwDt[6] + '</div></td>' +
-                        '<td><div style="color:black; right:inherit">' + rwDt[7] + '</div></td>' +
-                        '</tr>' + '</hr>');
+                    appendVal(rwDt, i);
                 }
             }
             var len = $('#tblRepoStock tbody tr').length;
@@ -143,7 +136,7 @@ function fucSearch() {
                 alert(cpnmsg.Response);
             }
         });
-    } 
+    }
     catch (e) {
     }
 
@@ -159,34 +152,76 @@ function getRepotItem(srcres) {
 }
 function btnSearch() {
     var strItem = $("#txt1").val();
-    var arrStr = [];
-    
-    var Len = $("#tblRepoStock tbody tr").length;
-    for(var i=0;i<Len-1;i++){
-      var chk =  $("#tblRepoStock tbody tr td:nth-child(1)").eq(i-1).text();
-      if (strItem == chk) {
-          var chk1 = $("#tblRepoStock tbody tr td:nth-child(2)").eq(i - 1).text();
-          var chk2 = $("#tblRepoStock tbody tr td:nth-child(3)").eq(i - 1).text();
-          var chk3 = $("#tblRepoStock tbody tr td:nth-child(4)").eq(i - 1).text();
-          var chk4 = $("#tblRepoStock tbody tr td:nth-child(5)").eq(i - 1).text();
-          var chk5 = $("#tblRepoStock tbody tr td:nth-child(6)").eq(i - 1).text();
-          var chk6 = $("#tblRepoStock tbody tr td:nth-child(7)").eq(i - 1).text();
-          var chk7 = $("#tblRepoStock tbody tr td:nth-child(8)").eq(i - 1).text();
-          $("#tblRepoStock tbody tr").remove();
-          Len = 1;
-          $("#tblRepoStock tbody").append(
-                       '<tr>' +
-                       '<td><div style="color:black; right:inherit">' + chk + '</div></td>' +
-                       '<td><div style="color:black; right:inherit">' + chk1 + '</div></td>' +
-                       '<td><div style="color:black; right:inherit">' + chk2 + '</div></td>' +
-                       '<td><div style="color:black; right:inherit">' + chk3 + '</div></td>' +
-                       '<td><div style="color:black; right:inherit">' + chk4 + '</div></td>' +
-                       '<td><div style="color:black; right:inherit">' + chk5 + '</div></td>' +
-                       '<td><div style="color:black; right:inherit">' + chk6 + '</div></td>' +
-                       '<td><div style="color:black; right:inherit">' + chk7 + '</div></td>' +
+    var chk = [];
+    var strCth;
+    var ddlSelect = $("#ddlSearch").val();
+    var tblLen = $("#tblRepoStock tbody tr").length;
+    for (var i = 0; i < tblLen - 1; i++) {
+        if (ddlSelect == 1) {
+            strCth = $("#tblRepoStock tbody tr td:nth-child(1)").eq(i - 1).text();
+        }
+        if (ddlSelect == 2) {
+            strCth = $("#tblRepoStock tbody tr td:nth-child(2)").eq(i - 1).text();
+        }
+        if (ddlSelect == 3) {
+            strCth = $("#tblRepoStock tbody tr td:nth-child(5)").eq(i - 1).text();
+        }
+        if (strItem == strCth) {
+            chk[0] = $("#tblRepoStock tbody tr td:nth-child(1)").eq(i - 1).text();
+            chk[1] = $("#tblRepoStock tbody tr td:nth-child(2)").eq(i - 1).text();
+            chk[2] = $("#tblRepoStock tbody tr td:nth-child(3)").eq(i - 1).text();
+            chk[3] = $("#tblRepoStock tbody tr td:nth-child(4)").eq(i - 1).text();
+            chk[4] = $("#tblRepoStock tbody tr td:nth-child(5)").eq(i - 1).text();
+            chk[5] = $("#tblRepoStock tbody tr td:nth-child(6)").eq(i - 1).text();
+            chk[6] = $("#tblRepoStock tbody tr td:nth-child(7)").eq(i - 1).text();
+            chk[7] = $("#tblRepoStock tbody tr td:nth-child(8)").eq(i - 1).text();
+            var tblLen2 = $("#tblRepoStock tbody tr").length + 1;
+
+            $("#tblRepoStock tr:last").after(
+                '<tr id=' + tblLen2 + '>' +
+                          '<td><div style="color:black; right:inherit">' + chk[0] + '</div></td>' +
+                          '<td><div style="color:black; right:inherit">' + chk[1] + '</div></td>' +
+                          '<td><div style="color:black; right:inherit">' + chk[2] + '</div></td>' +
+                          '<td><div style="color:black; right:inherit">' + chk[3] + '</div></td>' +
+                          '<td><div style="color:black; right:inherit">' + chk[4] + '</div></td>' +
+                          '<td><div style="color:black; right:inherit">' + chk[5] + '</div></td>' +
+                          '<td><div style="color:black; right:inherit">' + chk[6] + '</div></td>' +
+                          '<td><div style="color:black; right:inherit">' + chk[7] + '</div></td>' +
                        '</tr>' + '</hr>');
-         
-      }
+
+        }
+
     }
 
+    for (var j = 0; j <= tblLen - 1; j++) {
+        var x = j + 1;
+        $("#tblRepoStock tbody tr#" + x).remove();
+    }
+    var tblLen1 = $("#tblRepoStock tbody tr").length;
+    $('#label2').text(tblLen1);
+    closeSearch();
+}
+function appendVal(inChk, k) {
+    k = k + 1;
+    $("#tblRepoStock tbody").append(
+                           '<tr id=' + k + '>' +
+                           '<td><div style="color:black; right:inherit">' + inChk[0] + '</div></td>' +
+                           '<td><div style="color:black; right:inherit">' + inChk[1] + '</div></td>' +
+                           '<td><div style="color:black; right:inherit">' + inChk[2] + '</div></td>' +
+                           '<td><div style="color:black; right:inherit">' + inChk[3] + '</div></td>' +
+                           '<td><div style="color:black; right:inherit">' + inChk[4] + '</div></td>' +
+                           '<td><div style="color:black; right:inherit">' + inChk[5] + '</div></td>' +
+                           '<td><div style="color:black; right:inherit">' + inChk[6] + '</div></td>' +
+                           '<td><div style="color:black; right:inherit">' + inChk[7] + '</div></td>' +
+                           '</tr>' + '</hr>');
+}
+function closeSearch() {
+    $('#txt1').hide();
+    $('#btnSearch').hide();
+    $('#ddlSearch').hide();
+}
+function openSearch() {
+    $('#txt1').show();
+    $('#btnSearch').show();
+    $('#ddlSearch').show();
 }
